a7bed14c69ec3e004bb77ef751321c5af097e003
Mike123729 <konst.nikolas@gmail.com>
2022-06-28 10:59:09 +0300

    commit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,18 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleModal);
+//   refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.menu.classList.toggle("is-open");
+//   }
+// })();
+
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
